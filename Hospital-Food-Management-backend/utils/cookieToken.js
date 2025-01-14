a25f7bd6f5fe1b1a4e93f7825bdbb26d63408c0c
akshansh14 <akshanshsj14@gmail.com>
2025-01-15 00:35:59 +0530

    utils

--- Hospital-Food-Management-backend/utils/cookieToken.js
+++ Hospital-Food-Management-backend/utils/cookieToken.js
@@ -1,17 +1,21 @@
-const getJwtToken= require('../helpers/getJwtTokens')
+const getJwtToken = require("../helpers/getJwtTokens");
 
-const cookieToken=(user,res)=>{
-    const token = getJwtToken(user.id,user.password);
-    const options ={
-        expires: new Date(Date.now() + 3*24*60*60*1000),
-        httpOnly: true,
-    }
-    user.password=undefined;
-    res.status(200).cookie('token',token,options).json({
-        success:true,
-        user,
-        token
-    })
-}
+const cookieToken = (user, res) => {
+	const token = getJwtToken({
+		id: user.id,
+		role: user.role,
+		email: user.email,
+	});
+	const options = {
+		expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+		httpOnly: true,
+	};
+	user.password = undefined;
 
-module.exports=cookieToken;
+	// Set the cookie without sending a response
+	res.cookie("token", token, options);
+
+	return token;
+};
+
+module.exports = cookieToken;
